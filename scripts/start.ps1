4e02f7f0c6a967c47c138ba931529b314049d924
Skyler Severns <skyler@tetragensoftware.com>
2019-06-26 02:08:58 -0500

    Updating dependencies.

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -1,1 +1,1 @@
-java -jar eureka-1.0-SNAPSHOT.jar --server.port=8042 --server.address=localhost --server.tomcat.basedir=c:\\tmp\\tomcat3\\ --eureka.web.path=c:\\opt\\netflix\\eureka\\eureka-server-1.9.3
+java -jar eureka-1.0-SNAPSHOT.jar --server.port=8042 --server.address=0.0.0.0 --server.tomcat.basedir=c:\\tmp\\tomcat3\\ --eureka.web.path=c:\\opt\\netflix\\eureka-server-1.9.9
